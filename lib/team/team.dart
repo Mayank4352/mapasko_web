5e04bcc5646ba2bdce6fb9890d5c0557e1bed3d0
Madhav Gupta <madhav.gupta7674@gmail.com>
2024-09-11 10:55:10 +0530

    feat: Add abante teams

--- lib/team/team.dart
+++ lib/team/team.dart
@@ -8,12 +8,19 @@ class TeamPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map<String, String> imgPaths = {
+    Map<String, String> rwaImgPaths = {
+      'assets/pramod.png': 'Mr. Pramod Khullar\nPresident',
       "assets/sachdeva.png":
           "Lt. Colonel Darshan Sachdeva (Veteran) \nVice President",
       "assets/amandhull.png": "Er. Aman Dhull\nGeneral Secretary",
       "assets/nikhildhawan.png": "Mr. Nikhil Dhawan\nJoint Secretary",
       "assets/shavetagarg.png": "Mrs. Shaveta Garg\nTreasurer",
+    };
+    Map<String, String> abanteImgPaths = {
+      "assets/mosam.jpg": "Mr. Mosam Kumar\nOperations Manager",
+      "assets/amitkr.png": "Mr. Amit Kumar Yadav\nEstate Manager",
+      "assets/sandeep.jpg": "Mr. Sandeep Yadav\nTechnical Manager",
+      "assets/pooja.jpg": "Ms. Pooja Gupta\nHelp Desk"
     };
     return Scaffold(
       body: SingleChildScrollView(
@@ -22,7 +29,7 @@ class TeamPage extends StatelessWidget {
             const HomeAppBar(),
             const Text("OUR TEAM"),
             const Text(
-              "TEAM MEMBERS",
+              "RWA TEAM",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
             ),
             Padding(
@@ -39,10 +46,38 @@ class TeamPage extends StatelessWidget {
                   //  childAspectRatio: 0.7326,
                   // Aspect ratio of items (width / height)
                 ),
-                itemCount: imgPaths.length,
+                itemCount: rwaImgPaths.length,
                 itemBuilder: (context, index) {
                   return MemberWidget(
-                    imgPaths: imgPaths,
+                    imgPaths: rwaImgPaths,
+                    index: index,
+                  );
+                },
+                padding: const EdgeInsets.all(10),
+              ),
+            ),
+            const Text(
+              "ABANTE TEAM",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.sp),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisExtent: 40.h,
+
+                  crossAxisCount: 4, // Number of columns in the grid
+                  crossAxisSpacing: 10.sp, // Horizontal space between items
+                  mainAxisSpacing: 10.sp, // Vertical space between items
+                  //  childAspectRatio: 0.7326,
+                  // Aspect ratio of items (width / height)
+                ),
+                itemCount: rwaImgPaths.length,
+                itemBuilder: (context, index) {
+                  return MemberWidget(
+                    imgPaths: abanteImgPaths,
                     index: index,
                   );
                 },
