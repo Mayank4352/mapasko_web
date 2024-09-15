82e74765da890d485c09ef7df73ed37c7caaa6d7
Madhav Gupta <madhav.gupta7674@gmail.com>
2024-09-16 00:10:57 +0530

    feat: Responsiveness for calendar, added headings to document

--- lib/calender/calendar.dart
+++ lib/calender/calendar.dart
@@ -46,8 +46,8 @@ class _CalendarPageState extends State<CalendarPage> {
               scaffoldKey.currentState!.openEndDrawer();
             },
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+          SizedBox(
+            height: 5.h,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -100,87 +100,183 @@ class _CalendarPageState extends State<CalendarPage> {
             ),
           ),
           Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 60.w,
-                  child: CalendarControllerProvider(
-                    controller: _eventController,
-                    child: MonthView(
-                      key: ValueKey(_selectedMonth),
-                      controller: _eventController,
-                      minMonth: DateTime(1990),
-                      maxMonth: DateTime(2050),
-                      initialMonth:
-                          _selectedMonth, // Updated when dropdown changes
-                      cellAspectRatio: 1,
-                      onPageChange: (date, pageIndex) =>
-                          log("$date, $pageIndex"),
-                      onCellTap: (events, date) {
-                        // Callback when user taps on a cell
-                        log(events.toString());
-                      },
-                      startDay:
-                          WeekDays.sunday, // Change the first day of the week
-                      onEventTap: (event, date) => setState(() {
-                        eventDetails = event.title;
-                        eventDetails +=
-                            "\n\n${date.day}/${date.month}/${date.year}";
-                        eventDetails += "\n\n${event.description}";
-                      }),
-                      onEventDoubleTap: (events, date) =>
-                          log(events.toString()),
-                      onEventLongTap: (event, date) => log(event.toString()),
-                      onDateLongPress: (date) => log(date.toString()),
-                      headerBuilder:
-                          MonthHeader.hidden, // To hide the month header
-                      showWeekTileBorder: true,
-                      hideDaysNotInMonth: true,
-                      borderColor: Colors.black,
-                      borderSize: 2.sp,
+            child: 100.w < 900
+                ? SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 100.w < 900 ? 100.w : 100.h,
+                          width: 100.w < 900 ? 100.w : 60.w,
+                          child: CalendarControllerProvider(
+                            controller: _eventController,
+                            child: MonthView(
+                              key: ValueKey(_selectedMonth),
+                              controller: _eventController,
+                              minMonth: DateTime(1990),
+                              maxMonth: DateTime(2050),
+                              initialMonth:
+                                  _selectedMonth, // Updated when dropdown changes
+                              cellAspectRatio: 1,
+                              onPageChange: (date, pageIndex) =>
+                                  log("$date, $pageIndex"),
+                              onCellTap: (events, date) {
+                                // Callback when user taps on a cell
+                                log(events.toString());
+                              },
+                              startDay: WeekDays
+                                  .sunday, // Change the first day of the week
+                              onEventTap: (event, date) => setState(() {
+                                eventDetails = event.title;
+                                eventDetails +=
+                                    "\n\n${date.day}/${date.month}/${date.year}";
+                                eventDetails += "\n\n${event.description}";
+                              }),
+                              onEventDoubleTap: (events, date) =>
+                                  log(events.toString()),
+                              onEventLongTap: (event, date) =>
+                                  log(event.toString()),
+                              onDateLongPress: (date) => log(date.toString()),
+                              headerBuilder: MonthHeader
+                                  .hidden, // To hide the month header
+                              showWeekTileBorder: true,
+                              hideDaysNotInMonth: true,
+                              borderColor: Colors.black,
+                              borderSize: 2.sp,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100.w < 900 ? 100.w : 100.h,
+                          width: 100.w < 900 ? double.infinity : 40.w,
+                          child: Container(
+                            color: Colors.grey[200],
+                            child: Padding(
+                              padding: EdgeInsets.all(5.h),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: const Color(0xffff5e14),
+                                    height: 6.h,
+                                    width: double.infinity,
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Event Details:",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily:
+                                              GoogleFonts.nunito().fontFamily,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(2.h),
+                                    child: Text(
+                                      eventDetails,
+                                      style: TextStyle(
+                                          fontSize: 2.5.h,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily:
+                                              GoogleFonts.nunito().fontFamily),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.grey[200],
-                    child: Padding(
-                      padding: EdgeInsets.all(5.h),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            color: const Color(0xffff5e14),
-                            height: 6.h,
-                            width: double.infinity,
-                            alignment: Alignment.center,
-                            child: Text(
-                              "Event Details:",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: GoogleFonts.nunito().fontFamily,
-                                  color: Colors.white),
-                            ),
+                  )
+                : Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 100.w < 900 ? 100.w : 100.h,
+                        width: 100.w < 900 ? 100.w : 60.w,
+                        child: CalendarControllerProvider(
+                          controller: _eventController,
+                          child: MonthView(
+                            key: ValueKey(_selectedMonth),
+                            controller: _eventController,
+                            minMonth: DateTime(1990),
+                            maxMonth: DateTime(2050),
+                            initialMonth:
+                                _selectedMonth, // Updated when dropdown changes
+                            cellAspectRatio: 1,
+                            onPageChange: (date, pageIndex) =>
+                                log("$date, $pageIndex"),
+                            onCellTap: (events, date) {
+                              // Callback when user taps on a cell
+                              log(events.toString());
+                            },
+                            startDay: WeekDays
+                                .sunday, // Change the first day of the week
+                            onEventTap: (event, date) => setState(() {
+                              eventDetails = event.title;
+                              eventDetails +=
+                                  "\n\n${date.day}/${date.month}/${date.year}";
+                              eventDetails += "\n\n${event.description}";
+                            }),
+                            onEventDoubleTap: (events, date) =>
+                                log(events.toString()),
+                            onEventLongTap: (event, date) =>
+                                log(event.toString()),
+                            onDateLongPress: (date) => log(date.toString()),
+                            headerBuilder:
+                                MonthHeader.hidden, // To hide the month header
+                            showWeekTileBorder: true,
+                            hideDaysNotInMonth: true,
+                            borderColor: Colors.black,
+                            borderSize: 2.sp,
                           ),
-                          Padding(
-                            padding: EdgeInsets.all(2.h),
-                            child: Text(
-                              eventDetails,
-                              style: TextStyle(
-                                  fontSize: 2.5.h,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: GoogleFonts.nunito().fontFamily),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                        height: 100.w < 900 ? 100.w : 100.h,
+                        width: 100.w < 900 ? double.infinity : 40.w,
+                        child: Container(
+                          color: Colors.grey[200],
+                          child: Padding(
+                            padding: EdgeInsets.all(5.h),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  color: const Color(0xffff5e14),
+                                  height: 6.h,
+                                  width: double.infinity,
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "Event Details:",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily:
+                                            GoogleFonts.nunito().fontFamily,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(2.h),
+                                  child: Text(
+                                    eventDetails,
+                                    style: TextStyle(
+                                        fontSize: 2.5.h,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily:
+                                            GoogleFonts.nunito().fontFamily),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
           ),
         ],
       ),
